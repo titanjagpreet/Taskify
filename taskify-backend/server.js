8a120e223b4654e787da0194c49c09682e698e76
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-21 22:52:37 +0530

    Imported and used Routes

--- taskify-backend/server.js
+++ taskify-backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth");
+app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log("Listening to port....");
