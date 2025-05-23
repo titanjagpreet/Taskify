793e49b69058de259b85ff7459cf06978dd200a2
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-23 22:08:14 +0530

    Imported middlewares and used in Routes

--- taskify-backend/routes/authRoutes.js
+++ taskify-backend/routes/authRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.post("/signup");
+const validateSignup = require('../middlewares/validateSignup');
+const validateSignin = require('../middlewares/validateSignin');
 
-router.post("/signin");
+router.post("/signup", validateSignup);
+
+router.post("/signin", validateSignin);
 
 module.exports = router;
